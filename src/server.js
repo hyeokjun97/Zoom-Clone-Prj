cbcc92ac942eb85b7e8c03b4b91b87c4d2abbbfb
Hyeok Jun Kim <hyeokjun97@gmail.com>
2021-08-10 12:32:46 +0900

    Chat nearly complete

--- src/server.js
+++ src/server.js
@@ -25,10 +25,19 @@ const sockets = [];
 
 wss.on("connection", (socket) =>{
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to server✅");
     socket.on("close", ()=> console.log("Disconnected from the Browser❌"));
-    socket.on("message", (message)=>{
-        sockets.forEach((aSocket) => aSocket.send(message.toString('utf8')));
+    socket.on("message", (msg)=>{
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+
+        
     });
 });
 //"on" method waits "connection" to happens

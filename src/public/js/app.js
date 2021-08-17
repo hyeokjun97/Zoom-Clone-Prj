7511e2b18730eb132ba0475874da1321faf809e0
Hyeok Jun Kim <hyeokjun97@gmail.com>
2021-08-17 11:49:09 +0900

    Camera selection done

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,14 @@ async function getCameras(){
     try{
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(device => device.kind === "videoinput");
-        cameras.forEach(camera=>{
+        const currentCamera = myStream.getVideoTracks()[0];
+        cameras.forEach((camera)=>{
             const option = document.createElement("option");
             option.value = camera.deviceId;
             option.innerText = camera.label;
+            if(currentCamera.label === camera.label){
+                option.selected = true;
+            }
             camerasSelect.appendChild(option);
         });
     }catch(e){
@@ -25,13 +29,22 @@ async function getCameras(){
 }
 
 async function getMedia(deviceId){
+    const initialConstrains = {
+        audio: true,
+        video: {facingMode: "user"},
+    };
+    const cameraConstrains = {
+        audio: true,
+        video: {deviceId: {exact: deviceId} },
+    };
     try{
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        });
+        myStream = await navigator.mediaDevices.getUserMedia(
+                deviceId? cameraConstrains : initialConstrains
+            );
         myFace.srcObject = myStream;
-        await getCameras();
+        if(!deviceId){
+            await getCameras();
+        }
     } catch(e){
         console.log(e);
     }

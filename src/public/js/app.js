fd1134e5f9d42623322041a0486227f53106169a
Hyeok Jun Kim <hyeokjun97@gmail.com>
2021-08-18 11:30:04 +0900

    making RTC offers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -14,6 +14,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
 async function getCameras(){
     try{
@@ -92,10 +93,11 @@ camerasSelect.addEventListener("input", handleCameraChange);
 const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
-function startMedia(){
+async function startMedia(){
     welcome.hidden = true;
     call.hidden = false;
-    getMedia();
+    await getMedia();
+    makeConnection();
 }
 
 function handleWelcomeSubmit(event){
@@ -110,6 +112,20 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 //socket code
 
-socket.on("welcome", () =>{
-    console.log("Someone Joined");
+socket.on("welcome", async () =>{
+    
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("Sent the offer");
+    socket.emit("offer", offer, roomName);
 });
+socket.on("offer", (offer)=>{
+    console.log(offer);
+})
+
+//RTC Code
+
+function makeConnection(){
+    myPeerConnection = new RTCPeerConnection();
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
